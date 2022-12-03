58063069ebaf9d62e8e7bd1a02175148870e7d7a
Guilherme Wilias <113528431+guilhermewilias@users.noreply.github.com>
2022-12-03 14:46:25 -0300

    Add files via upload

---
+++ at_11_ex01_sem1_c.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main (){
+
+
+int num1, num2, som;
+
+printf ("Entre com o primeiro numero : ");
+scanf ("\n%d", &num1);
+
+printf ("\nEntre com o segundo numero : ");
+scanf ("\n%d", &num2);
+
+som = num1 + num2;
+
+printf ("\nNumeros entrados foram : %d e %d ", num1, num2);
+printf ("\nSoma dos numeros : %d ", som);
+
+return 0;
+
+}
+
